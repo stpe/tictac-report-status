1798aaf9083c71a1ccf4f930d7375629e1043650
Stefan Pettersson <stefpet@gmail.com>
2014-08-08 17:33:21 +0200

    API wrapper

---
+++ app/modules/tictacApi.js
@@ -0,0 +1,64 @@
+var moment = require('moment');
+var needle = require('needle');
+var Promise = require('es6-promise').Promise;
+var querystring = require('querystring');
+
+var TICTAC_API_BASE_URL = 'https://www.tictacmobile.com/webservice/rest/';
+
+var apiGet = function(method, params) {
+    params = params || {};
+    params.userid = process.env.TICTAC_API_USERID;
+    params.password = process.env.TICTAC_API_PASSWORD;
+
+    var url = TICTAC_API_BASE_URL + method + '.jsp?' + querystring.stringify(params);
+
+    if (process.env.TICTAC_DEBUG) {
+        console.log('API ' + method + ': ' + querystring.stringify(params, ',', '='));
+    }
+
+    return new Promise(function(resolve, reject) {
+        needle.get(url,
+            function(err, resp, data) {
+                if (err) {
+                    return reject(Error(err));
+                }
+
+                if (process.env.TICTAC_DEBUG) {
+                    console.log('API response:');
+                    console.log(JSON.stringify(data, null, 2));
+                }
+                resolve(data);
+            }
+        );
+    });
+};
+
+// Returns time summed up for all users or one specific user
+exports.getUserReport = function(startDate, endDate) {
+    return apiGet('getUserReport', {
+        startdate: moment(startDate).format('YYYY-MM-DD'),
+        enddate: moment(endDate).format('YYYY-MM-DD')
+    });
+};
+
+// Returns all time, cost, expence and sales entries for the given period for a given project
+exports.getProjectRows = function(projectId, startDate, endDate) {
+    return apiGet('getUserReport', {
+        projectId: projectId,
+        startdate: moment(startDate).format('YYYY-MM-DD'),
+        enddate: moment(endDate).format('YYYY-MM-DD')
+    });
+};
+
+// Returns normal time for the given period
+exports.getNormalTime = function(startDate, endDate) {
+    return apiGet('getNormaltime', {
+        startdate: moment(startDate).format('YYYY-MM-DD'),
+        enddate: moment(endDate).format('YYYY-MM-DD')
+    });
+};
+
+// Returns all projects and activities in the account. Or the [number] most recently created or updated projects
+exports.getProjects = function() {
+    return apiGet('getProjects');
+};
---
+++ test/tictacApi.js
@@ -0,0 +1,45 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
+var chai = require('chai');
+var should = chai.should();
+chai.use(require("chai-as-promised"));
+
+var moment = require('moment');
+var parseXml = require('xml2js').parseXml;
+
+describe('Tictac API', function() {
+    var api = require('../app/modules/tictacApi');
+
+    var projectId;
+
+    it('should retrieve user report', function() {
+        return api.getUserReport(
+            moment().subtract(1, 'month').startOf('month'),
+            moment().subtract(1, 'month').endOf('month')
+        ).should.eventually.have.property('TimeSumPerUserAndProject');
+    });
+
+    it('should retrieve projects', function() {
+        return api.getProjects().should.eventually.have.property('TicTacProjects')
+            .then(function(projects) {
+                // get id of first project
+                projectId = projects.TicTacProject[0].projid;
+            });
+    });
+
+    it('should retrieve project rows', function() {
+        return api.getProjectRows(
+                projectId,
+                moment().subtract(1, 'month').startOf('month'),
+                moment().subtract(1, 'month').endOf('month')
+            ).should.eventually.have.property('TimeSumPerUserAndProject');
+    });
+
+    it('should retrieve normal time', function() {
+        return api.getNormalTime(
+            moment().subtract(1, 'month').startOf('month'),
+            moment().subtract(1, 'month').endOf('month')
+        ).should.eventually.have.property('TicTacNormalTime');
+    });
+});
