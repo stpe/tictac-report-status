dadc9fad90e8a88fb75693d0a395d7eb2154d4c4
Stefan Pettersson <stefpet@gmail.com>
2014-08-18 00:04:52 +0200

    Using nock to mock

--- test/tictacApi.js
+++ test/tictacApi.js
@@ -6,16 +6,61 @@ var should = chai.should();
 chai.use(require("chai-as-promised"));
 
 var moment = require('moment');
+var nock = require('nock');
 
 describe('Tictac API', function() {
     var api = require('../app/modules/tictacApi');
 
+    nock(api.TICTAC_API_BASE_URL)
+        .filteringPath(/webservice\/rest\/getProjectRows\.jsp\?projectId=(\d+).*/, 'getProjectRows$1')
+        .get('/getProjectRows1344344083404')
+        .replyWithFile(200, __dirname + '/data/getProjectRows1344344083404.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getProjectRows1345410092429')
+        .replyWithFile(200, __dirname + '/data/getProjectRows1345410092429.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getProjectRows1393961115410')
+        .replyWithFile(200, __dirname + '/data/getProjectRows1393961115410.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getProjectRows1398845875122')
+        .replyWithFile(200, __dirname + '/data/getProjectRows1398845875122.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getProjectRows1399919145449')
+        .replyWithFile(200, __dirname + '/data/getProjectRows1399919145449.xml', {
+            'content-type': 'application/xml'
+        });
+
+    nock(api.TICTAC_API_BASE_URL)
+        .filteringPath(/webservice\/rest\/(\w+)\.jsp?.*/, '$1')
+        .get('/getUsers')
+        .replyWithFile(200, __dirname + '/data/getUsers.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getNormaltime')
+        .replyWithFile(200, __dirname + '/data/getNormaltime.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getProjects')
+        .replyWithFile(200, __dirname + '/data/getProjects.xml', {
+            'content-type': 'application/xml'
+        })
+        .get('/getUserReport')
+        .replyWithFile(200, __dirname + '/data/getUserReport.xml', {
+            'content-type': 'application/xml'
+        });
+
+    var startDate = moment("2014-07-01");
+    var endDate = moment("2014-07-31");
     var projectId;
 
     it('should retrieve user report', function() {
         return api.getUserReport(
-            moment().subtract(1, 'month').startOf('month'),
-            moment().subtract(1, 'month').endOf('month')
+            startDate,
+            endDate
         ).should.eventually.have.property('TimeSumPerUserAndProject');
     });
 
@@ -29,16 +74,16 @@ describe('Tictac API', function() {
 
     it('should retrieve project rows', function() {
         return api.getProjectRows(
-                moment().subtract(1, 'month').startOf('month'),
-                moment().subtract(1, 'month').endOf('month'),
+                startDate,
+                endDate,
                 projectId
             ).should.eventually.have.property('TicTacRows');
     });
 
     it('should retrieve normal time', function() {
         return api.getNormaltime(
-            moment().subtract(1, 'month').startOf('month'),
-            moment().subtract(1, 'month').endOf('month')
+            startDate,
+            endDate
         ).should.eventually.have.property('TicTacNormalTime');
     });
 
