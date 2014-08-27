b636c667ff9ce2753232f537852dda431c90b333
Stefan Pettersson <stefpet@gmail.com>
2014-08-27 09:32:17 +0200

    Refactor API mocking into separate module

--- test/dateRange.js
+++ test/dateRange.js
@@ -1,18 +1,10 @@
 var chai = require('chai');
 var should = chai.should();
 var moment = require('moment');
-var nock = require('nock');
 
 describe('dateRange', function() {
     var dateRange = require('../app/modules/dateRange');
     var api = require('../app/modules/tictacApi');
-
-    nock(api.TICTAC_API_BASE_URL)
-        .filteringPath(/webservice\/rest\/(\w+)\.jsp?.*/, '$1')
-        .get('/getNormaltime')
-        .replyWithFile(200, __dirname + '/data/getNormaltime.xml', {
-            'content-type': 'application/xml'
-        });
 
     it('should return range of days with normaltime', function() {
         var startDate = moment("2014-07-01");
