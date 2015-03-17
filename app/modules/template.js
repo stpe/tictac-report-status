a30d294fbb5111e43673f6465696aa1606e7afe7
Stefan Pettersson <stefpet@gmail.com>
2015-03-17 17:34:32 +0100

    Avoid warning with duplicate variable name

--- app/modules/template.js
+++ app/modules/template.js
@@ -1,7 +1,7 @@
 var handlebars = require('handlebars');
 var fs = require('fs');
 
-exports.generate = function(data, template) {
-    var template = fs.readFileSync(__dirname + "/../template/" + template + ".handlebars", "utf8");
+exports.generate = function(data, templateName) {
+    var template = fs.readFileSync(__dirname + "/../template/" + templateName + ".handlebars", "utf8");
     return (handlebars.compile(template))(data);
 };
