ad308c590a89c3e05728dd6472886ac447698750
Stefan Pettersson <stefpet@gmail.com>
2015-03-18 13:31:55 +0100

    Inline CSS for email

--- app/modules/sendEmail.js
+++ app/modules/sendEmail.js
@@ -1,3 +1,4 @@
+var juice = require('juice');
 var sendgrid = require('sendgrid')(
     process.env.SENDGRID_USERNAME,
     process.env.SENDGRID_PASSWORD
@@ -6,11 +7,14 @@ var sendgrid = require('sendgrid')(
 var DATE_FORMAT = "YYYY-MM-DD";
 
 exports.send = function(email, body, startDate, endDate) {
+    // inline all css
+    var inlinedHtml = juice(body);
+
     sendgrid.send({
         to: email,
         from: process.env.FROM_EMAIL,
         subject: "Tidrapport " + startDate.format(DATE_FORMAT) + " - " + endDate.format(DATE_FORMAT),
-        html: body
+        html: inlinedHtml
     }, function(err, json) {
         if (err) {
             console.error("SendGrid error", err);
